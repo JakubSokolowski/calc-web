4fb4c0a8c9e37d6258ebe3838c07c4e5705b15a6
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-11-28 23:49:50 +0100

    Update repo link #35

--- apps/calc-web/src/app/components/repo-link/repo-link.tsx
+++ apps/calc-web/src/app/components/repo-link/repo-link.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { IconButton, Tooltip } from '@material-ui/core';
 import { repoUrl } from '../../../assets/env/meta';
-import { GitlabIcon } from '@calc/ui';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import { useTranslation } from 'react-i18next';
 
 
@@ -11,7 +11,7 @@ export const RepoLink: FC = () => {
     return (
         <Tooltip title={t('appBar.repo')}>
             <IconButton href={repoUrl}>
-                <GitlabIcon/>
+                <GitHubIcon/>
             </IconButton>
         </Tooltip>
     );
