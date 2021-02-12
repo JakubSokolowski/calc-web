3bf9ceea2f14395f2162bbda6fc66e3c3579b2f4
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-02-12 02:07:35 +0100

    Add reference to requirements in addition tests

--- apps/calc-web-e2e/src/integration/positional/addition.spec.ts
+++ apps/calc-web-e2e/src/integration/positional/addition.spec.ts
@@ -1,6 +1,6 @@
 import {
-    getAdditionResult,
-    getOperationGrid,
+    getAdditionResult, getCellByCoords,
+    getOperationGrid, gridHasProperResultRow,
     operationReturnsProperResult
 } from '../../support/positional-calculator';
 import { OperationTemplate } from '@calc/positional-calculator';
@@ -13,6 +13,7 @@ describe('Addition', () => {
         cy.visit('#/tools/positional/positional-calculator');
     });
 
+    // STUD_REQ_5_1
     it('should add two decimal numbers together', () => {
         const config: OperationTemplate<AlgorithmType> = {
             operands: ['123', '321'],
@@ -28,6 +29,83 @@ describe('Addition', () => {
         getOperationGrid().toMatchSnapshot();
     });
 
+    // STUD_REQ_5_2
+    it('should add two decimal numbers in complement representation', () => {
+        const base = 10;
+        const config: OperationTemplate<AlgorithmType> = {
+            operands: ['(9)123', '321'],
+            operation: OperationType.Addition,
+            algorithm: AdditionType.Default,
+            base
+        };
+        const expected = '-556';
+        const expectedComplement = '(9)444';
+
+        operationReturnsProperResult(config, expected);
+
+        getAdditionResult().toMatchSnapshot();
+        getOperationGrid().toMatchSnapshot();
+
+        gridHasProperResultRow(expectedComplement, base, 4, 2);
+    });
+
+    // STUD_REQ_5_2
+    it('should add negative number, using complement representation', () => {
+        const base = 10;
+        const config: OperationTemplate<AlgorithmType> = {
+            operands: ['123', '-321'],
+            operation: OperationType.Addition,
+            algorithm: AdditionType.Default,
+            base
+        };
+        const expected = '-198';
+        const expectedComplement = '(9)802';
+
+        operationReturnsProperResult(config, expected);
+
+        getAdditionResult().toMatchSnapshot();
+        getOperationGrid().toMatchSnapshot();
+
+        gridHasProperResultRow(expectedComplement, base, 4, 3);
+    });
+
+    // STUD_REQ_5_3
+    it.only('should show proper position details and carries', () => {
+        const base = 10;
+        const config: OperationTemplate<AlgorithmType> = {
+            operands: ['987', '6354'],
+            operation: OperationType.Addition,
+            algorithm: AdditionType.Default,
+            base
+        };
+        const expected = '7341';
+
+        operationReturnsProperResult(config, expected);
+
+        getAdditionResult().toMatchSnapshot();
+        getOperationGrid().toMatchSnapshot();
+
+        // grid should grid display with 3 carries
+        getCellByCoords(2, 0).contains('1');
+        getCellByCoords(3, 0).contains('1');
+        getCellByCoords(4, 0).contains('1');
+
+        // should display details with position result and carries
+        getCellByCoords(5, 3).trigger('mouseover')
+            .getByDataTest('add-at-position')
+            .contains('C_{1}=1')
+            .contains('S_{0}=1');
+
+        getCellByCoords(4, 3).trigger('mouseover')
+            .getByDataTest('add-at-position')
+            .contains('C_{2}=1')
+            .contains('S_{1}=4');
+
+        gridHasProperResultRow(expected, base, 5, 3);
+    });
+
+
+    // STUD_REQ_5_4
     it('should add multiple binary numbers together', () => {
         const config: OperationTemplate<AlgorithmType> = {
             operands: [
@@ -44,6 +122,23 @@ describe('Addition', () => {
         };
 
         const expected = '-10110101100010100'; // -92948
+
+        operationReturnsProperResult(config, expected);
+
+        getAdditionResult().toMatchSnapshot();
+        getOperationGrid().toMatchSnapshot();
+    });
+
+    // STUD_REQ_5_4
+    it('should add 10 numbers together', () => {
+        const config: OperationTemplate<AlgorithmType> = {
+            operands: new Array(10).fill('10'),
+            operation: OperationType.Addition,
+            algorithm: AdditionType.Default,
+            base: 10
+        };
+
+        const expected = '100';
 
         operationReturnsProperResult(config, expected);
 
