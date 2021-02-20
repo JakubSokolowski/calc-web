646677eb91df1b05dc0c43dbf566716f311b2cbc
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-02-20 17:42:27 +0100

    Add sentry config

--- apps/calc-web/src/main.tsx
+++ apps/calc-web/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './app/app';
 import { Provider } from 'react-redux';
 import { store } from '@calc/core';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+    dsn: 'https://70bd26cd51234ad587dd3b9b2864f8a0@o528188.ingest.sentry.io/5645379',
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <Provider store={store}>
