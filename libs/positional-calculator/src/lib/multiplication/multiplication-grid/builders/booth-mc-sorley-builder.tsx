01a75d1d736409bd7b6aa3265d4bde7d9c8d006d
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-04-04 22:14:08 +0200

    Group results shifted on hover:
    - RC: for cases when number of multipier fractions digits
      was even, various indices for group cells and placements
      were incorrect
    - fix: extract the even/odd branches to separate methods
      and fix indices (fugly)

--- libs/positional-calculator/src/lib/multiplication/multiplication-grid/builders/booth-mc-sorley-builder.tsx
+++ libs/positional-calculator/src/lib/multiplication/multiplication-grid/builders/booth-mc-sorley-builder.tsx
@@ -11,56 +11,16 @@ export class BoothMcSorleyBuilder extends BoothBuilder {
         index: number,
         outputIndex: number
     ): CellGroup {
+        const evenNumOfFractionDigits = Math.max(this.info.maxOperandsFractionDigits) % 2 === 0;
 
-        if(output.isPaddingDigit) {
-            return {
-                cells: []
-            }
+        if (evenNumOfFractionDigits) {
+            return this.buildGroupForEven(group, output, index, outputIndex);
+        } else {
+            return this.buildGroupForOdd(group, output, index, outputIndex);
         }
-        const rowCellsThatAreMultiplied = this.getCellsToAdd(output);
-        const { totalWidth } = this.info;
-        const yOffset = this.getMultiplierRowStartOffset();
-        const groupOffset = 2 * index + 1;
-
-        const isPartialOutputGroup = group.output.length === 1;
-
-
-        const trigger1: CellConfig = {
-            x: totalWidth - groupOffset,
-            y: 1 + yOffset,
-            preventGroupTrigger: outputIndex === 0 && !isPartialOutputGroup
-        };
-
-        const trigger2: CellConfig = {
-            x: totalWidth - groupOffset - 1,
-            y: 1 + yOffset,
-            preventGroupTrigger: outputIndex === 1 && !isPartialOutputGroup
-        };
-
-        const rowOffset = isPartialOutputGroup ? 0 : outputIndex === 0 ? 1 : 0;
-        const rowIndex = 2* index + rowOffset;
-        const resultRow = this.buildMultiplyByDigitResultRow(rowIndex);
-        const sdGroupCells = this.getMultiplierSDGroupCells(trigger1, group);
-
-        return {
-            cells: [
-                ...rowCellsThatAreMultiplied,
-                ...resultRow,
-                ...sdGroupCells,
-                trigger1,
-                trigger2,
-            ],
-            contentProps: {},
-            popoverPlacement: index === 0 ? 'right' : 'left',
-            anchorPosition: index === 0 ? trigger1 : trigger2,
-            contentBuilder: () => <BoothMcSorleyGroupResult result={group} outputIndex={outputIndex} />,
-        };
     }
 
-    protected getMultiplierSDGroupCells(
-        trigger: CellConfig,
-        group: SDConversionGroupResult
-    ): CellConfig[] {
+    protected getMultiplierSDGroupCells(trigger: CellConfig, group: SDConversionGroupResult): CellConfig[] {
         const groupStart: CellConfig = {
             x: trigger.x - 1,
             y: trigger.y - 1,
@@ -71,5 +31,86 @@ export class BoothMcSorleyBuilder extends BoothBuilder {
         };
 
         return groupCellsInStraightLine(groupStart, groupEnd, true);
+    }
+
+    private buildGroupForOdd(
+        group: SDConversionGroupResult,
+        output: SDGroupDigit,
+        index: number,
+        outputIndex: number
+    ): CellGroup {
+        if (output.isPaddingDigit) {
+            return {
+                cells: [],
+            };
+        }
+
+        const { totalWidth } = this.info;
+        const yOffset = this.getMultiplierRowStartOffset();
+        const groupOffset = 2 * index;
+
+        const trigger1: CellConfig = {
+            x: totalWidth - groupOffset,
+            y: 1 + yOffset,
+            preventGroupTrigger: index === 0 ? false : outputIndex === 0,
+        };
+
+        const trigger2: CellConfig = {
+            x: totalWidth - groupOffset - 1,
+            y: 1 + yOffset,
+            preventGroupTrigger: index === 0 ? false : outputIndex === 1,
+        };
+
+        const rowIndex = index === 0 ? 0 : 2 * index - outputIndex;
+        const resultRow = this.buildMultiplyByDigitResultRow(rowIndex);
+        const rowCellsThatAreMultiplied = this.getCellsToAdd(output);
+        const sdGroupCells = this.getMultiplierSDGroupCells(trigger1, group);
+
+        return {
+            cells: [...rowCellsThatAreMultiplied, ...resultRow, ...sdGroupCells, trigger1, trigger2],
+            contentProps: {},
+            popoverPlacement: index === 0 ? 'right' : 'left',
+            anchorPosition: trigger2,
+            contentBuilder: () => <BoothMcSorleyGroupResult result={group} outputIndex={outputIndex} />,
+        };
+    }
+
+    private buildGroupForEven(
+        group: SDConversionGroupResult,
+        output: SDGroupDigit,
+        index: number,
+        outputIndex: number
+    ): CellGroup {
+        const rowCellsThatAreMultiplied = this.getCellsToAdd(output);
+        const { totalWidth } = this.info;
+        const yOffset = this.getMultiplierRowStartOffset();
+        const groupOffset = 2 * index + 1;
+
+        const isPartialOutputGroup = group.output.length === 1;
+
+        const trigger1: CellConfig = {
+            x: totalWidth - groupOffset,
+            y: 1 + yOffset,
+            preventGroupTrigger: outputIndex === 0 && !isPartialOutputGroup,
+        };
+
+        const trigger2: CellConfig = {
+            x: totalWidth - groupOffset - 1,
+            y: 1 + yOffset,
+            preventGroupTrigger: outputIndex === 1 && !isPartialOutputGroup,
+        };
+
+        const rowOffset = isPartialOutputGroup ? 0 : outputIndex === 0 ? 1 : 0;
+        const rowIndex = 2 * index + rowOffset;
+        const resultRow = this.buildMultiplyByDigitResultRow(rowIndex);
+        const sdGroupCells = this.getMultiplierSDGroupCells(trigger1, group);
+
+        return {
+            cells: [...rowCellsThatAreMultiplied, ...resultRow, ...sdGroupCells, trigger1, trigger2],
+            contentProps: {},
+            popoverPlacement: index === 0 ? 'right' : 'left',
+            anchorPosition: index === 0 ? trigger1 : trigger2,
+            contentBuilder: () => <BoothMcSorleyGroupResult result={group} outputIndex={outputIndex} />,
+        };
     }
 }
