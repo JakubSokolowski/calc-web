c096d8d9eb7faa252787e6560b216e977addcbc1
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-10-14 22:39:08 +0200

    Extract extended-select-item component

--- libs/common-ui/src/lib/components/extended-select/extended-select.tsx
+++ libs/common-ui/src/lib/components/extended-select/extended-select.tsx
@@ -1,8 +1,7 @@
 import React, { ChangeEvent, ReactNode } from 'react';
-import { IconButton, MenuItem, styled, TextField, Tooltip } from '@mui/material';
-import HelpIcon from '@mui/icons-material/Help';
+import { MenuItem, styled, TextField } from '@mui/material';
 import type { ExtendedOption } from '../../core/models/extended-option';
-import { useTranslation } from 'react-i18next';
+import { ExtendedSelectItem } from './extended-select-item';
 
 
 interface P<T> {
@@ -16,36 +15,16 @@ const PREFIX = "ExtendedSelect";
 
 const classes = {
     select: `${PREFIX}-select`,
-    item: `${PREFIX}-item`,
-    spacer: `${PREFIX}-spacer`,
-    infoButton: `${PREFIX}-infoButton`,
 };
 
 const Root = styled('div')(({ theme }) => ({
     [`& .${classes.select}`]: {
         minWidth: '180px'
     },
-
-    [`& .${classes.item}`]: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: '100%'
-    },
-
-    [`& .${classes.spacer}`]: {
-        flexGrow: 1
-    },
-
-    [`& .${classes.infoButton}`]: {
-        cursor: 'default',
-        pointerEvents: 'initial'
-    },
 }));
 
 
 export const ExtendedSelect = <T extends ExtendedOption>(props: P<T> & { children?: ReactNode }) => {
-    const {t} = useTranslation();
     const {options, onChange, value, label, ...rest} = props;
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -57,25 +36,7 @@ export const ExtendedSelect = <T extends ExtendedOption>(props: P<T> & { childre
     const items = options.map((option) => {
         return (
             <MenuItem key={option.type} value={option.type} disabled={option.disallowed}>
-                <div className={classes.item}>
-                    <div>
-                        {t(option.tKey)}
-                    </div>
-                    <div className={classes.spacer}/>
-                    {
-                        option.disallowed &&
-                        <Tooltip title={option.disallowedReason}>
-                            <IconButton
-                                onClick={(e) => e.stopPropagation()}
-                                size={'small'}
-                                className={classes.infoButton}
-                                disabled={false}
-                            >
-                                <HelpIcon/>
-                            </IconButton>
-                        </Tooltip>
-                    }
-                </div>
+                <ExtendedSelectItem option={option}/>
             </MenuItem>
         );
     });
