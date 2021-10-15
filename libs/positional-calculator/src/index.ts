a465c7b2b5e393487932b8b2bb5ea5448d5626a9
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-10-15 21:40:02 +0200

    Fix typescript build warnings #196

--- libs/positional-calculator/src/index.ts
+++ libs/positional-calculator/src/index.ts
@@ -3,4 +3,4 @@ export * from './lib/positional-calculator-view/positional-calculator-view';
 export { OperationRenderer} from './lib/operation-renderer/operation-renderer';
 export { ComplementDetailsRenderer } from './lib/complement-converter-view/complement-details-renderer/complement-details-renderer';
 export { ConversionRenderer } from '../../base-converter/src/lib/components/conversion-renderer/conversion-renderer';
-export { OperationTemplate } from './lib/core/models/operation-template';
+export type { OperationTemplate } from './lib/core/models/operation-template';
