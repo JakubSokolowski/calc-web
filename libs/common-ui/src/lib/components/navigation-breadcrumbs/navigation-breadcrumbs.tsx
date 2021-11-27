55d3e7344deb698a17c3c970bb4c9b890f822725
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-11-27 15:58:50 +0100

    Yep, fixing breadcrumbs now

--- libs/common-ui/src/lib/components/navigation-breadcrumbs/navigation-breadcrumbs.tsx
+++ libs/common-ui/src/lib/components/navigation-breadcrumbs/navigation-breadcrumbs.tsx
@@ -3,6 +3,7 @@ import { Breadcrumbs, Link, styled, Typography } from '@mui/material';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import { replaceAll } from '@calc/utils';
 import { useTranslation } from 'react-i18next';
+import { environment } from '@calc/env';
 
 const PREFIX = 'Breadcrumbs';
 
@@ -43,6 +44,7 @@ interface P {
 export const NavigationBreadcrumbs: FC<P> = ({ path, theoryPath }) => {
     const { t } = useTranslation();
     const pathFragments = path.split('/').filter(r => !!r);
+    const deployPrefix = environment.deployUrl ? `/${environment.deployUrl}` : '';
 
     const subRoutesCombinations = pathFragments.map((_, index) => {
         const all = pathFragments.slice(0, index + 1);
@@ -64,7 +66,7 @@ export const NavigationBreadcrumbs: FC<P> = ({ path, theoryPath }) => {
             );
         } else {
             return (
-                <Link key={idx} color="inherit" href={`/#/${s}`}>
+                <Link key={idx} color="inherit" href={`/${deployPrefix}/#/${s}`}>
                     {translation.title || translation}
                 </Link>
             );
@@ -82,7 +84,7 @@ export const NavigationBreadcrumbs: FC<P> = ({ path, theoryPath }) => {
                 {
                     theoryPath &&
                     <Breadcrumbs>
-                        <Link className={classes.link} color="inherit" href={`#${theoryPath}`}>
+                        <Link className={classes.link} color="inherit" href={`/${deployPrefix}/#${theoryPath}`}>
                             <MenuBookIcon className={classes.linkIcon}/>
                             {t('common.theory')}
                         </Link>
